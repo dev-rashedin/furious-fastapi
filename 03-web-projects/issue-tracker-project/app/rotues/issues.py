b16fd01e86f99c3061c42e4a62e313d5a20385d4
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-18 06:10:36 +0600

    issues and storage.py created

---
+++ 03-web-projects/issue-tracker-project/app/rotues/issues.py
@@ -0,0 +1,10 @@
+import uuid
+from fastapi import APIRouter, HTTPException, status
+
+
+router = APIRouter(prefix="api/v1/issues", tags=["issues"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_issue():
+  return {"id": str(uuid.uuid4())}
