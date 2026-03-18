e3915862b0ad689c7a045174cb1c4dd7c4aa8c38
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-18 06:22:50 +0600

    main.py updated

--- 03-web-projects/issue-tracker-project/app/rotues/issues.py
+++ 03-web-projects/issue-tracker-project/app/rotues/issues.py
@@ -1,8 +1,8 @@
 import uuid
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, status
 
 
-router = APIRouter(prefix="api/v1/issues", tags=["issues"])
+router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
--- 03-web-projects/issue-tracker-project/main.py
+++ 03-web-projects/issue-tracker-project/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from app.rotues.issues import router as issues_router
 
 
-app = FastAPI()
+app = FastAPI(
+  title="Issue Tracker",
+  description="Issue Tracker API",
+  version="0.0.1",
+  docs_url="/docs",
+  redoc_url="/redoc"
+)
+
+@app.get("/")
+def root():
+  return {"message": "Hello! This is the Issue Tracker API"}
 
 @app.get("/health")
 async def root():
   return {"status": "ok"}
+
+
+app.include_router(issues_router)
