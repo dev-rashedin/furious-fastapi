23cc61c8b996bc56c4d510de695472ec3934a9f0
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-19 11:04:54 +0600

    issue.py updated

---
+++ 02-experiments/app/data/items.py
@@ -0,0 +1,9 @@
+items = [
+  {"id": 1, "name": "Samsung Galaxy S21", "price": 1000, "description": "A high configuration phone"},
+ {"id": 2, "name": "Apple iPhone 13", "price": 1200, "description": "A high configuration phone"},
+ {"id": 3, "name": "Google Pixel 6", "price": 1400, "description": "A high configuration phone"},
+ {
+  "id": 4, "name": "Macbook Pro", "price": 2000, "description": "A high configuration Laptop"
+}
+]
+
--- 03-web-projects/issue-tracker-project/app/rotues/issues.py
+++ 03-web-projects/issue-tracker-project/app/rotues/issues.py
@@ -8,16 +8,10 @@ router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
 
 
-# Get all the issues
-@router.get("/", response_model=list[IssueOut])
-async def get_issues():
-  """ Get all the issues """
-  issues = load_data()
-  return issues
-
 # Create a new issue
 @router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
 async def create_issue(payload: IssueCreate):
+
   """ Create a new issue """
   issues = load_data()
   new_issue = {
@@ -32,3 +26,25 @@ async def create_issue(payload: IssueCreate):
   save_data(issues)
 
   return new_issue
+
+
+
+# Get all the issues
+@router.get("/", response_model=list[IssueOut])
+async def get_issues():
+  """ Get all the issues """
+  issues = load_data()
+  return issues
+
+
+# Get a single issue
+@router.get("/{issue_id}", response_model=IssueOut)
+async def get_single_issue(issue_id: str):
+  """ Get a single issue """
+  issues = load_data()
+
+  for issue in issues:
+    if issue["id"] == issue_id:
+      return issue
+  
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Issue not found")
