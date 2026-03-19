f1be78ae9ea0535d377ce2623c38bd0f90fc9184
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-19 10:49:48 +0600

    issue routes updated

--- 03-web-projects/issue-tracker-project/app/rotues/issues.py
+++ 03-web-projects/issue-tracker-project/app/rotues/issues.py
@@ -1,10 +1,30 @@
 import uuid
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate, IssueOut, IssueUpdate
+from app.storage import load_data, save_data
 
 
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+
+# Get all the issues
+@router.get("/", response_model=list[IssueOut])
+async def get_issues():
+  """ Get all the issues """
+  issues = load_data()
+  return issues
+
+# Create a new issue
+@router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
 async def create_issue():
+  """ Create a new issue """
+  issues = load_data()
+  new_issue = {
+    "id": str(uuid.uuid4()),
+    "title": "New issue",
+    "description": "New issue description",
+    "priority": "low",
+    "status": "open"
+  }
   return {"id": str(uuid.uuid4())}
