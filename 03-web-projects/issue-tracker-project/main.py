c462bac619a7f40173939f7ae9032af65f044f56
dev-rashedin <rashedinislam.06@gmail.com>
2026-03-19 22:52:10 +0600

    cors middleware added and requirements.txt generated

--- 03-web-projects/issue-tracker-project/main.py
+++ 03-web-projects/issue-tracker-project/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.rotues.issues import router as issues_router
 from app.middleware.timer import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI(
@@ -12,6 +14,14 @@ app = FastAPI(
 )
 
 app.middleware("http")(timing_middleware)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
